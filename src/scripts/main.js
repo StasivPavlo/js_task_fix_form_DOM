b46c27de0e795426ea1b4c7746f3c85b27ab61f1
pavlostasiv <pavlostasiv@gmail.com>
2023-05-06 14:39:04 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+const addLabels = (form) => {
+  const inputs = form.querySelectorAll('input');
+
+  inputs.forEach(input => {
+    const label = document.createElement('label');
+
+    label.className = 'field-label';
+    label.textContent = input.name;
+    label.setAttribute('for', input.id);
+
+    input.placeholder = input.name;
+    input.before(label);
+  });
+};
+
+const forms = document.querySelectorAll('form');
+const signOnForm = forms[0];
+const signInForm = forms[1];
+
+addLabels(signInForm);
+addLabels(signOnForm);
